b61f7e38c4450a942c8eae89cddd9106e79ebc8b
Marcus <marcu@LAPTOP-J77NCVB0>
2024-02-22 17:22:48 +0100

    ReadFile funktion

--- Losenordshanterare/FileService.cs
+++ Losenordshanterare/FileService.cs
@@ -8,6 +8,26 @@ namespace Losenordshanterare
 {
     internal class FileSerivce
     {
+        public static void Execute()
+        {
+            string filePath = "test.txt"; // Ändra till riktiga filnamnet när det är bestämt
+            string fileContent = ReadFile(filePath);
+            Console.WriteLine("File content:");
+            Console.WriteLine(fileContent);
+        }
 
+        static string ReadFile(string path)
+        {
+            try
+            {
+                string content = File.ReadAllText(path);
+                return content;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return null; // Ändra null till något annat?
+            }
+        }
     }
 }
--- Losenordshanterare/Program.cs
+++ Losenordshanterare/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Losenordshanterare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSerivce.Execute();
+            Console.ReadLine();
+        }
+    }
+}
