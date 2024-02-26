21c9a2520d9e891e8eea323c13bcfc96e3520c85
Lukas <lukas.lindqvist31@gmail.com>
2024-02-26 13:56:51 +0100

    Added aes encryption

---
+++ Losenordshanterare/Encryption.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using Crypto = System.Security.Cryptography;
+using System.Runtime.Intrinsics.X86;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+
+
+
+namespace Losenordshanterare
+{
+    internal class Encryption
+    {
+        private readonly Crypto.Aes _aes;
+
+        public byte[] Encrypt(string data)
+        {
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(ms, _aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    csEncrypt.Write(byteArray, 0, data.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public byte[] Decrypt(string data)
+        {
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(ms, _aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    csDecrypt.Write(byteArray, 0, data.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+    } 
+}
