b35328264fe17f11e937ab64ba0eeb8fb2fa0a50
Lukas <lukas.lindqvist31@gmail.com>
2024-02-27 11:00:14 +0100

    Added Vault class without function to decrypt vault

---
+++ Vault.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Vault
+{
+    private Dictionary<string, string> Vault = new Dictionary<string, string>;
+    private VaultKey vaultKey;
+    private string path;
+
+    public Vault(string path, VaultKey vaultkey)
+    {
+        this.path = path;
+        this.vaultKey = vaultkey;
+
+        if (!File.Exists(path))
+        {
+            File.Create(path).Close();
+            try
+            {
+                byte[] vaultBytes = File.ReadAllBytes(path);
+
+                if (vaultBytes.Lenght > 0)
+                {
+                    //Funktionalitet för att låsa upp valvet 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"The following error occured {ex}");
+            }     
+        }
+    }
+}
