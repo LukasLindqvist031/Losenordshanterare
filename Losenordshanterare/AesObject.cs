776c8ca6eac6162d55db80b086f87363b10b9a26
LukasLindqvist031 <115667263+LukasLindqvist031@users.noreply.github.com>
2024-02-27 12:33:30 +0100

    Added an AES class object

--- Losenordshanterare/AesObject.cs
+++ Losenordshanterare/AesObject.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 public class AesObject
 {
     public byte[] Key { get; set; }
-    public byte[] Value { get; set; }
+    public byte[] IV { get; set; }
     public CipherMode Mode { get; set; }
+    public PaddingMode Padding { get; set; }
 
     public AesObject(Aes aes)
-	{
+    {
         Key = aes.Key;
         IV = aes.IV;
         Mode = aes.Mode;
+        Padding = aes.Padding;
     }
 }
