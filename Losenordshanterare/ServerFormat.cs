8a8e57ae6916898bb1b85c307342e8d7ad1a4f50
LukasLindqvist031 <115667263+LukasLindqvist031@users.noreply.github.com>
2024-02-29 09:03:38 +0100

    Added class to help with dictionary formatting

--- Losenordshanterare/ServerFormat.cs
+++ Losenordshanterare/ServerFormat.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class ServerFormat
+namespace Losenordshanterare
 {
-	public ServerFormat()
-	{
-        public string IV { get; set; } 
+    internal class ServerFormat
+    {
+        public byte[] IV { get; set; } 
         public Dictionary<string, string> PropertyPassword { get; set; } 
+
     }
 }
